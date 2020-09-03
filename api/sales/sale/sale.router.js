6113dc41e29fe115f7004ffb6c9d325c1f7929cc
Hailo <jose.mallma@pucp.pe>
2020-09-03 10:59:19 -0500

    :sparkles: List service for sales

--- api/sales/sale/sale.router.js
+++ api/sales/sale/sale.router.js
@@ -7,5 +7,6 @@ const Validator = require('./sale.validator');
 const router = express.Router();
 
 router.post('/', celebrate(Validator.Post), Controller.postSale);
+router.get('/', celebrate(Validator.List), Controller.listSale);
 
 module.exports = router;
