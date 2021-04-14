75ba575d67da61bfd680dbbe943b780571b2b539
Hugo <hufedebe@gmail.com>
2021-04-14 18:27:12 -0500

    update delete service

--- api/inventory/product/product.service/deleteProduct.js
+++ api/inventory/product/product.service/deleteProduct.js
@@ -1,4 +1,4 @@
-const { Product, ProductBox } = require('@dbModels');
+const { Product, ProductBox, SuppliedProduct, ProformaProduct } = require('@dbModels');
 const { setResponse } = require('../../../utils');
 
 const deleteProduct = async reqParams => {
@@ -9,6 +9,20 @@ const deleteProduct = async reqParams => {
     where: { productId: reqParams.id },
   });
   if (productBox) return setResponse(400, 'Se tiene cajas del producto.');
+
+  const suppliedProduct = await SuppliedProduct.findOne({
+    where: { productId: reqParams.id },
+  });
+  if (suppliedProduct)
+    return setResponse(400, 'Se tiene un proceso de abastecimiento pendiente.');
+
+  const proformaProduct = await ProformaProduct.findOne({
+    where: { productId: reqParams.id },
+  });
+  if (proformaProduct)
+    return setResponse(400, 'Se tiene el producto en una proforma.');
+
+  const productDelete = await Product.destroy({ where: { id: reqParams.id } });
 
   return setResponse(200, 'Delete Product.', product);
 };
