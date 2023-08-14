019f88cd84d08c5345c44e5e168005fb1a3bf1cc
Reyner Loza <reyner.loza@leniolabs.com>
2023-08-14 01:57:15 -0500

    remove unused

--- api/utils/constants/utils.js
+++ api/utils/constants/utils.js
@@ -1,11 +1,6 @@
 const _ = require('lodash');
 
-const HOSTNAME_DESA = 'appdesa.maximportaciones.com.pe';
-const HOSTNAME_PROD = 'app.maximportaciones.com.pe';
-
 module.exports = {
-  getHostName: () =>
-    process.NODE_ENV === 'production' ? HOSTNAME_PROD : HOSTNAME_DESA,
   getDictValues: dict => {
     return Object.keys(dict).map(function(key) {
       return _.get(dict[key], 'value', dict[key]);
